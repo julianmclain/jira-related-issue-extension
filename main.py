f52d369533b686897427dc1f97f29e9006294848
julianmclain <julianrmclain@gmail.com>
2023-11-11 16:57:44 -0800

    format

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from sentence_transformers.util import cos_sim
 
 import os
 
-
 load_dotenv()
 
 JIRA_HOST = "https://iterable.atlassian.net/"
@@ -29,61 +28,114 @@ jira = JIRA(
 )
 
 # JIRA settings
-children = True #@param {type:"boolean"}
-ancestors = True #@param {type:"boolean"}
-issuetypes = True #@param {type:"boolean"}
-names = True #@param {type: "boolean"}
-changelog = False #@param {type:"boolean"}
-renderedFields = False #@param {type:"boolean"}
-operations = False #@param {type:"boolean"}
-editmeta = False #@param {type:"boolean"}
-versionedRepresentations = False #@param {type:"boolean"}
+children = True  # @param {type:"boolean"}
+ancestors = True  # @param {type:"boolean"}
+issuetypes = True  # @param {type:"boolean"}
+names = True  # @param {type: "boolean"}
+changelog = False  # @param {type:"boolean"}
+renderedFields = False  # @param {type:"boolean"}
+operations = False  # @param {type:"boolean"}
+editmeta = False  # @param {type:"boolean"}
+versionedRepresentations = False  # @param {type:"boolean"}
 
-EXPAND_NAMES = set([
-    "issuetypes", "names","ancestors","children","operations",
-    "versionedRepresentations","editmeta","changelog","renderedFields"
-    ])
-API_OPTIONS = ",".join([f for f in EXPAND_NAMES if globals()[f]])
-FIELD_MAP = {"_".join(f['name'].lower().split()): f['key'] for f in jira.fields() }
-FIELDS = [
-    'key',
-    'summary',
-    'issuetype',
-    'components',
-    'created',
-    'resolved',
-    'description',
-    'labels',
-    'priority',
-    'comment'
+EXPAND_NAMES = set(
+    [
+        "issuetypes",
+        "names",
+        "ancestors",
+        "children",
+        "operations",
+        "versionedRepresentations",
+        "editmeta",
+        "changelog",
+        "renderedFields",
     ]
-FIELD_LIST = sorted([FIELD_MAP.get(f,f) for f in FIELDS])
+)
+API_OPTIONS = ",".join([f for f in EXPAND_NAMES if globals()[f]])
+FIELD_MAP = {"_".join(f["name"].lower().split()): f["key"] for f in jira.fields()}
+FIELDS = [
+    "key",
+    "summary",
+    "issuetype",
+    "components",
+    "created",
+    "resolved",
+    "description",
+    "labels",
+    "priority",
+    "comment",
+]
+FIELD_LIST = sorted([FIELD_MAP.get(f, f) for f in FIELDS])
+
+
+def save_all_jira_issues(db) -> int:
+    total_hits = 0
+    last_hits = None
+
+    while last_hits != 0: 
+        issues = query_jira(
+            "type = 'On Call Question' and created > -180d and created < -30d", 10, total_hits 
+        )
+        issue_summaries = [create_and_save_issue_summary(db, i) for i in issues]
+
+        hits = len(issues) 
+        total_hits += hits
+        last_hits = hits
+        print(f"processed {last_hits} hits")
+
+    return total_hits
 
 
 def query_jira(qs: str, max_results: int = 1, start_at: int = 0):
-    return jira.search_issues(qs, maxResults=max_results, startAt=start_at, expand=API_OPTIONS,
-                            fields=FIELD_LIST)
+    return jira.search_issues(
+        qs,
+        maxResults=max_results,
+        startAt=start_at,
+        expand=API_OPTIONS,
+        fields=FIELD_LIST,
+    )
 
 
-def create_issue_summary(issue: Issue, include_comments=True):
+def create_and_save_issue_summary(db, issue: Issue, include_comments=True):
     raw_issue = issue.raw
-    fields = raw_issue['fields']
-    summary_items = [raw_issue['key']]
-    summary_items.append ((fields['summary'] or ""))
-    summary_items.append ((fields['description'] or ""))
-    summary_items += fields['labels']
-    summary_items += [f['name'] for f in fields['components']]
+    fields = raw_issue["fields"]
+    summary_items = [raw_issue["key"]]
+    summary_items.append((fields["summary"] or ""))
+    summary_items.append((fields["description"] or ""))
+    summary_items += fields["labels"]
+    summary_items += [f["name"] for f in fields["components"]]
     if include_comments:
-        summary_items += [f['body'] for f in fields['comment']['comments']]
+        summary_items += [f["body"] for f in fields["comment"]["comments"]]
 
-    return ". ".join(summary_items)
+    summary_string = ". ".join(summary_items)
+    db.execute(
+        "INSERT INTO issues (key, summary) values (?, ?)",
+        (raw_issue["key"], summary_string)
+    )
+    db.commit()
+    return summary_string
 
+def insert_issue(db, key: str, summary: str):
+    pass
 
+def create_issue_table(db) -> None:
+    db.execute(
+        """
+            CREATE TABLE IF NOT EXISTS issues(
+                key TEXT NOT NULL,
+                summary TEXT NOT NULL,
+                embedding BLOB
+            )
+        """
+    )
 
 
 if __name__ == "__main__":
-    # issues = jira.search_issues("type = 'On Call Question' and created > -180d and created < -30d", 10, 0)
-    # issue_summaries = [create_issue_summary(i) for i in issues]
+    db = sqlite3.connect("jira-issue-embeddings.db")
+    db.enable_load_extension(True)
+    create_issue_table(db)
+    save_all_jira_issues(db)
+    # create all embeddings
 
     # model = SentenceTransformer('all-MiniLM-L6-v2')
     # embeddings = model.encode(issue_summaries)
@@ -92,13 +144,9 @@ if __name__ == "__main__":
     #     print("Embedding:", embedding)
     #     print("")
 
-    db = sqlite3.connect(':memory:')
-    db.enable_load_extension(True)
     sqlite_vss.load(db)
-
-    version, = db.execute('select vss_version()').fetchone()
-    print(version)
 
     # next steps
     # - store tickets in sqlite so you don't need to query every time
     # - test https://github.com/asg017/sqlite-vss/tree/main/bindings/python
+    db.close
